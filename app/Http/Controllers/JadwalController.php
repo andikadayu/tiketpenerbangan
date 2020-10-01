9b68ddc9f5274c51a40a9e29356528ae696ccbf9
Muhammad Andika <dikapolk@gmail.com>
2020-10-01 14:30:31 +0700

    Adding Update and Get Data Function in Controller

--- app/Http/Controllers/JadwalController.php
+++ app/Http/Controllers/JadwalController.php
@@ -36,4 +36,43 @@ class JadwalController extends Controller
             ));
         }
     }
+
+    function getting(Request $request)
+    {
+        $jadwal_model= new JadwalPenerbangan();
+
+        return $jadwal_model->getting($request->get('id')); 
+    }
+
+    function process_update(Request $request)
+    {
+        $jadwal_model = new JadwalPenerbangan();
+
+        $pesawat = $request->post('pesawat');
+        $asal = $request->post('asal');
+        $tujuan = $request->post('tujuan');
+        $jadwal = $request->post('jadwal');
+        $id= $request->post('id_jadwal');
+
+        $update=array();
+        $update['id_pesawat'] = $pesawat;
+        $update['id_bandara_asal'] = $asal;
+        $update['id_bandara_tujuan'] = $tujuan;
+        $update['tgl_jadwal'] = date('Y-m-d', strtotime($jadwal));
+
+
+        $process = $jadwal_model->upd($id,$update);
+
+        if ($process) {
+            echo json_encode(array(
+                'RESULT' => 'OK',
+                'MESSAGE' => 'Success to update data to table'
+            ));
+        } else {
+            echo json_encode(array(
+                'RESULT' => 'FAILED',
+                'MESSAGE' => 'Failed to update data to table'
+            ));
+        }
+    }
 }
