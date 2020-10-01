964d560a92eacb87cef53b1d7fe778bdc40feeda
Muhammad Andika <dikapolk@gmail.com>
2020-10-01 14:31:20 +0700

    Adding Update and Get Data in Model

--- app/JadwalPenerbangan.php
+++ app/JadwalPenerbangan.php
@@ -8,11 +8,12 @@ use PDO;
 
 class JadwalPenerbangan extends Model
 {
+
     public function getJadwalPenerbangan()
     {
         $pdo = DB::getPdo();
 
-        $sql = "SELECT b.nama_pesawat, c.nama_bandara AS bandara_asal, d.nama_bandara AS bandara_tujuan, a.tgl_jadwal FROM jadwal_penerbangan a JOIN pesawat b ON b.id_pesawat = a.id_pesawat JOIN bandara c ON c.id_bandara = a.id_bandara_asal JOIN bandara d ON d.id_bandara = a.id_bandara_tujuan";
+        $sql = "SELECT id_jadwal , b.nama_pesawat, c.nama_bandara AS bandara_asal, d.nama_bandara AS bandara_tujuan, a.tgl_jadwal FROM jadwal_penerbangan a JOIN pesawat b ON b.id_pesawat = a.id_pesawat JOIN bandara c ON c.id_bandara = a.id_bandara_asal JOIN bandara d ON d.id_bandara = a.id_bandara_tujuan";
 
         $statement = $pdo->prepare($sql);
         $statement->execute();
@@ -24,4 +25,17 @@ class JadwalPenerbangan extends Model
     {
         return DB::table('jadwal_penerbangan')->insert($data);
     }
+
+    public function getting($id)
+    {
+        return DB::table('jadwal_penerbangan')
+            ->where('id_jadwal',$id)
+            ->get();
+    }
+
+    public function upd($id, $upd = array())
+    {
+        return DB::table('jadwal_penerbangan')->where('id_jadwal',$id)->update($upd);
+    }
+
 }
