69a5e9f1da960273accb93db3122501ac95830c6
Muhammad Andika <dikapolk@gmail.com>
2020-10-12 19:35:44 +0700

    Add Model Bandara

--- app/Bandara.php
+++ app/Bandara.php
@@ -7,6 +7,12 @@ use Illuminate\Support\Facades\DB;
 
 class Bandara extends Model
 {
+
+	protected $table = 'bandara';
+	protected $primaryKey = 'id_bandara';
+	protected $keyType = 'string';
+    public $timestamps = null;
+
     public function getDataBandara()
     {
         return DB::table('bandara')->get();
